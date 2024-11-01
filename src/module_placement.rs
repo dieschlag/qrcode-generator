73388e81b059b85f1af365ed2a7cc0c9a6e05a91
m.dischler <m.dischler@nanocorp.fr>
2024-11-01 19:23:32 +0100

    changed test and commented function

--- src/module_placement.rs
+++ src/module_placement.rs
@@ -20,45 +20,40 @@ impl Chain {
     }
 }
 
+/// Third step in QR Code generation before masking data.
+/// Module placement uses a 1-D vector to represent data. This vector represents a square matrix of size n, with a new line every n elements in the vector.
+/// The number 1 corresponds to black modules and 0 to white modules.
 pub(crate) fn module_placement(data: Vec<u8>) {
-    // data is the result from previous step (here: the adding of reed-soloms error correction codewords)
-    // We define, which is simply the len of qrcode, here
-    let n = 21;
+    //
 
+    println!("**************************** Module Placement ****************************\n");
+
+    // ======================================= Initialization =======================================
+
+    // We define, which is simply the len of qrcode, TODO: make it dynamical
+    let n = 21;
+    println!("Len of QR Code is: {}\n", n);
+
+    // Vector containing each bit of the bit stream composed of encoded message + error correction codewords
     let mut bits: Vec<u8> = Vec::new();
     for value in data.iter() {
-        let mut v = *value; // Create a mutable copy to shift
+        let mut v = *value; // Creates a mutable copy to shift
         for _ in 0..8 {
-            bits.push((v & 128) >> 7);
+            bits.push((v & 128) >> 7); // AND with 128 to extract dominant bit
             v <<= 1; // Shift left to prepare for the next bit
         }
     }
-    println!("Len: {}", bits.len());
-    let mut single_line_bytes = String::new();
 
-    for bite in data {
-        single_line_bytes.push_str(&format!("{bite:08b}"));
-    }
-
-    println!("Bits on a single line: {single_line_bytes}");
-    let mut test = String::new();
-    for i in (0..4).rev() {
-        for j in 0..(n - 9) {
-            test.push_str(&bits[2 * i * (n - 9) + j].to_string());
-            test.push_str(&bits[(2 * i + 1) * (n - 9) + j].to_string());
-        }
-    }
-    println!("{test}");
-    println!("Vector bits: {:?}", bits); // Prints each bit, start
-
+    // Initiate Chain instance
     let mut bits = Chain::new(bits);
 
-    // First we define the size of our vector, here we have a 21 x 21 for a 1-version with error correction L
+    // Initiate result vector
     let mut result: Vec<u8> = vec![0; n * n];
 
-    // Then we place function paterns: here only finder, timinig patterns, seperators and dark module
+    // ======================================= Patterns placement =======================================
 
     // ===== Finder paterns =====
+
     //Horizontal
     for i in 0..7 {
         result[i] = 1;
@@ -68,6 +63,7 @@ pub(crate) fn module_placement(data: Vec<u8>) {
         result[(n - 1) * n + i] = 1;
         result[(n - 7) * n + i] = 1;
     }
+
     // Vertical
     for i in 1..6 {
         result[i * n] = 1;
@@ -77,6 +73,7 @@ pub(crate) fn module_placement(data: Vec<u8>) {
         result[(n - 7 + i) * n] = 1;
         result[(n - 7 + i) * n + 6] = 1;
     }
+
     // Center
     for i in 0..3 {
         result[(2 * n) + (i + 2)] = 1;
@@ -91,17 +88,19 @@ pub(crate) fn module_placement(data: Vec<u8>) {
     }
 
     //  ===== Timing Patterns =====
+
     for i in 7..(n - 7) {
         result[i * n + 6] = ((i + 1) % 2) as u8;
         result[6 * n + i] = ((i + 1) % 2) as u8;
     }
 
     // ===== Dark Module =====
+
     result[(n - 7) * n + 8] = 1;
 
-    // ===== Placing data inside matrix =====
+    // ======================================= Data placement =======================================
 
-    // First four columns (space under the right finder pattern)
+    // ===== Space under right finder =====
     for i in 0..4 {
         if i % 2 == 0 {
             for j in 0..(n - 9) {
@@ -116,7 +115,9 @@ pub(crate) fn module_placement(data: Vec<u8>) {
             }
         }
     }
-    // Filling space between finder patterns and avoiding horizontal timing pattern
+
+    // ===== Space bewtween two finders =====
+
     for i in 0..2 {
         if i % 2 == 1 {
             for j in 0..n {
@@ -136,13 +137,15 @@ pub(crate) fn module_placement(data: Vec<u8>) {
         }
     }
 
-    // Filling space between vertical timing and rest of data
+    // ===== Filling gap before vertical timing =====
+
     for j in 0..4 {
         result[(n - 9 - j) * n + 8] = bits.next().unwrap();
         result[(n - 9 - j) * n + 7] = bits.next().unwrap();
     }
 
-    // Filling space between the two left finder patterns-
+    // ===== Filling space before the two right finder patterns =====
+
     for i in 0..3 {
         if i % 2 == 0 {
             for j in 0..4 {
@@ -161,8 +164,9 @@ pub(crate) fn module_placement(data: Vec<u8>) {
     display(&result, n);
 }
 
-// We define a display function for debug purposes and encoding visualiton
+/// Used to display the values inside the matrix
 fn display(list: &Vec<u8>, n: usize) {
+    println!("Result matrix is:");
     for i in 0..n {
         for j in 0..n {
             // line.push_str(&format!(" {} ", &list[i * n + j].to_string()));
@@ -171,8 +175,3 @@ fn display(list: &Vec<u8>, n: usize) {
         println!("\n");
     }
 }
-
-// // We define a function to access an element in vector at given position (x,y) in matrix, considering indexing begins at 0, n is still the matrix dimension
-// fn get_element(list: Vec<u8>, x: u8, y: u8, n: u8) {
-//     list.get(x*n + y)
-// }
