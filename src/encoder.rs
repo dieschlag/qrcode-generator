30d6cf2d3fc8ef231144aeff6d8f720e1bf4843a
m.dischler <m.dischler@nanocorp.fr>
2024-11-01 16:05:56 +0100

    code cleanup

--- src/encoder.rs
+++ src/encoder.rs
@@ -1,16 +1,24 @@
 use std::error::Error;
 
-pub(crate) fn encoder(text: String) -> Vec<u8> {
-    let copy = text.clone().into_bytes();
+pub(crate) fn encoder(text: String) -> Result<Vec<u8>, Box<dyn Error>> {
+    println!("**************************** Encoding Data ****************************\n");
+    println!("Initial text data: {} \n", text);
+
+    // Converting string to a sequence of bytes with length encoded in it
     let mut bytes = text.clone().into_bytes();
-    let length: u8 = text.len().try_into().unwrap();
+    let length: u8 = text.len().try_into()?; //Need to be a u8 for a 1L
     bytes.insert(0, length);
+
+    // Initiate result vector, containing a stream of bites divided in u8, with length, method indicator and data
     let mut result: Vec<u8> = Vec::new();
 
+    // Encoding first bits: 0100, indicating binary encoding is used
     let first_byte = bytes[0];
+    // Pushing 0100 at the beginning of first octet with first four bits of first data octet
     let first_result_byte = (0b0100 << 4) | (first_byte >> 4);
     result.push(first_result_byte);
 
+    // Shifting all bits in u8 since to compensate shift at the beggining
     for i in 0..bytes.len() - 1 {
         let current_byte = bytes[i];
         let next_byte = bytes[i + 1];
@@ -18,40 +26,27 @@ pub(crate) fn encoder(text: String) -> Vec<u8> {
         result.push(combined_byte);
     }
 
+    // Shifting last byte
     let last_byte = bytes[bytes.len() - 1];
     let last_combined_byte = (last_byte & 0b00001111) << 4;
-
     result.push(last_combined_byte);
 
+    // Making sure that the length of final bit strean matches qrcode requirements (here 19 data words)
+    // We complete the final sequence with the pattern 11101100 00010001, corresponding to 236 and 17 respectively
     while result.len() < 19 {
-        if let Some(&last) = result.last() {
-            // Si le dernier élément est 236, ajouter 17
-            if last == 236 {
-                result.push(17);
-            } else {
-                result.push(236);
-            }
+        if result.last().unwrap() == &236 {
+            result.push(17);
+        } else {
+            result.push(236);
         }
     }
-    let mut single_line_bytes = String::new();
-    println!("Print each byte \n");
-    for bite in result.clone() {
-        println!("{bite:08b}");
-        single_line_bytes.push_str(&format!("{bite:08b}"));
+
+    // Displaying result as a single bit stream
+    println!("Final encoding as a single bit stream: ");
+    for byte in result {
+        print!("{byte:08b}");
     }
+    println!();
 
-    println!("Print bits as single string \n");
-    println!("{single_line_bytes}");
-
-    println!("Compare the two :");
-
-    let mut copy_single_line_bytes = String::new();
-    for bite in copy {
-        copy_single_line_bytes.push_str(&format!("{bite:08b}"));
-    }
-
-    println!("original: {copy_single_line_bytes}");
-    println!("modified: {single_line_bytes}");
-
-    return result;
+    return Ok(result);
 }
--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,6 @@ mod encoder;
 mod reed_solomon;
 
 fn main() {
-    // println!("{:?}", reed_solomon());
-    // encoder(String::from("hello world"))
-    let result = reed_solomon(vec![
-        17, 236, 17, 236, 17, 236, 64, 67, 77, 220, 114, 209, 120, 11, 91, 32,
-    ]);
-    println!("result: {:?}", result);
-    // println!("{}", 256 ^ 285)
+    let string = "hello world";
+    encoder(String::from(string));
 }
