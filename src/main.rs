3a8e3a61a66f879c685ce1ab23681fa5fa5eaa00
m.dischler <m.dischler@nanocorp.fr>
2024-11-01 18:38:16 +0100

    use of encoded data in reed_solomon

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,7 @@ mod reed_solomon;
 fn main() {
     // TODO: Error management
     let string = "hello world";
-    let _encoded_data = encoder(String::from(string));
-    let mut test = vec![
-        17, 236, 17, 236, 17, 236, 64, 67, 77, 220, 114, 209, 120, 11, 91, 32,
-    ];
-    test.reverse();
-    let ecc_words = reed_solomon(test);
+    let encoded_data = encoder(String::from(string)).unwrap();
+    let ecc_words = reed_solomon(encoded_data);
     println!("{ecc_words:?}");
 }
