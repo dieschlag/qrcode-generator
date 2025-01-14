1b178d404685e5e18a7bce71b245e687fca1be2c
m.dischler <m.dischler@nanocorp.fr>
2025-01-14 20:54:55 +0100

    correct line spacing

--- src/data_masking/mod.rs
+++ src/data_masking/mod.rs
@@ -21,7 +21,7 @@ pub(crate) fn data_masking(data: Vec<u8>) {
     let two_mask = two_mask(data.clone(), n);
     display(&two_mask, n);
     let two_score = evaluate_masking(two_mask);
-    println!("Second Score is: {}", two_score);
+    println!("Second Score is: {}\n", two_score);
 
     println!("Third mask \n");
     let three_mask = three_mask(data.clone(), n);
@@ -33,31 +33,31 @@ pub(crate) fn data_masking(data: Vec<u8>) {
     let four_mask = four_mask(data.clone(), n);
     display(&four_mask, n);
     let four_score = evaluate_masking(four_mask);
-    println!("Fourth Score is: {}", four_score);
+    println!("Fourth Score is: {}\n", four_score);
 
     println!("Fifth mask \n");
     let five_mask = five_mask(data.clone(), n);
     display(&five_mask, n);
     let five_score = evaluate_masking(five_mask);
-    println!("Fifth Score is: {}", five_score);
+    println!("Fifth Score is: {}\n", five_score);
 
     println!("Sixth mask \n");
     let six_mask = six_mask(data.clone(), n);
     display(&six_mask, n);
     let six_score = evaluate_masking(six_mask);
-    println!("Sixth Score is: {}", six_score);
+    println!("Sixth Score is: {}\n", six_score);
 
     println!("Seventh mask \n");
     let seven_mask = seven_mask(data.clone(), n);
     display(&seven_mask, n);
     let seven_score = evaluate_masking(seven_mask);
-    println!("Seventh Score is: {}", seven_score);
+    println!("Seventh Score is: {}\n", seven_score);
 
     println!("Eigth mask \n");
     let eigth_mask = eigth_mask(data.clone(), n);
     display(&eigth_mask, n);
     let eigth_score = evaluate_masking(eigth_mask);
-    println!("Eight Score is: {}", eigth_score);
+    println!("Eight Score is: {}\n", eigth_score);
 }
 
 #[cfg(test)]
