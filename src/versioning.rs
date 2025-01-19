fef1eb96327f483a2133b336f246a9c07f6b0b57
m.dischler <m.dischler@nanocorp.fr>
2025-01-19 15:12:05 +0100

    change of computer

--- src/versioning.rs
+++ src/versioning.rs
@@ -1,4 +1,5 @@
-pub(crate) fn version_bits(error_correction: &str, mask_number: u32) -> Vec<u8> {
+pub(crate) fn version_bits(error_correction: &str, mask_number: u8) -> Vec<u8> {
+    let result = 0 as u16;
     let error_correction_bits: u8 = match error_correction {
         "L" => 1,
         "M" => 0,
@@ -7,6 +8,8 @@ pub(crate) fn version_bits(error_correction: &str, mask_number: u32) -> Vec<u8> 
         _ => 10,
     };
 
-    let mask_pattern_bits
+    let mask_bits = mask_number;
+    let format: u8 = mask_bits | (error_correction_bits << 3);
+    println!("{:08b}", format);
     vec![]
 }
