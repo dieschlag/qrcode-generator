8257819b64114a21dd7201128ead8ee2a4e1973e
m.dischler <m.dischler@nanocorp.fr>
2025-01-20 07:54:07 +0100

    generation of qrcode as a jpeg

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use data_masking::data_masking;
 use encoder::encoder;
+use image_generation::image_generation;
 use module_placement::module_placement;
 use reed_solomon::reed_solomon;
 use versioning::{place_version_bits, version_bits};
 
 mod data_masking;
 mod encoder;
+mod image_generation;
 mod module_placement;
 mod reed_solomon;
 mod versioning;
@@ -25,5 +27,6 @@ fn main() {
     // data_masking(matrix);
     let data_with_mask = data_masking(matrix);
     let version_bits = version_bits("L", 4);
-    place_version_bits(data_with_mask, version_bits);
+    let final_matrix = place_version_bits(data_with_mask, version_bits);
+    image_generation(final_matrix);
 }
