32d19d9c0fd70a149baebafee6b26eb176e0d94a
dieschlag <a@a.com>
2025-06-09 21:45:23 +0700

    cleaner main

--- src/main.rs
+++ src/main.rs
@@ -14,20 +14,35 @@ mod versioning;
 
 fn main() {
     // TODO: Error management
-    let string = "hello";
-    let encoded_data = encoder(String::from(string)).unwrap();
-    let ecc_data = reed_solomon(encoded_data.clone(), 7);
-    println!("{ecc_data:?}");
 
+    // ------------------------------------------------
+    //
+    // For now, it is only possible to generate 1L QR Code. To generate a QR code, update the string bellow,
+    // and verify it is possible to generate a 1L QR code with it.
+    //
+    // ------------------------------------------------
+
+    let input = "hello";
+
+    let encoded_data = encoder(String::from(input)).unwrap();
+
+    let ecc_data = reed_solomon(encoded_data.clone(), 7);
+
+    println!("{ecc_data:?}");
     println!("");
+
     let full_data = [encoded_data, ecc_data].concat();
+
     println!("Full data in bits");
+
     for bit in full_data.clone() {
         print!("{bit:08b}");
     }
+
     let matrix = module_placement(full_data);
     let (data_with_mask, mask_number) = data_masking(matrix);
     let version_bits = version_bits("L", mask_number);
     let final_matrix = place_version_bits(data_with_mask, version_bits);
+
     image_generation(final_matrix);
 }
